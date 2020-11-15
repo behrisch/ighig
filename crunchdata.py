6e65065172cc2a4c472424374f93de8137e1712a
Michael Behrisch <oss@behrisch.de>
2020-11-15 12:30:53 +0100

    adding label colors and milestones

--- crunchdata.py
+++ crunchdata.py
@@ -3,21 +3,31 @@
 import os
 import sys
 import json
-import pandas as pd
 import csv
 import re
 
-columns = ['number', 'time', 'closed', 'by', 'label', 'label_color', 'assignee']
-root = sys.argv[1] if len(sys.argv) > 1 else "github-backup/repositories/sumo/issues"
-with open('out.csv', 'w') as out:
-    print(','.join(columns), file=out)
+root = sys.argv[1] if len(sys.argv) > 1 else "github-backup/repositories/sumo/"
+with open('issues.csv', 'w') as out:
+    print('issue,time,closed,by,label,assignee', file=out)
     writer = csv.writer(out)
-    for issueFile in os.listdir(root):
-        issue = json.load(open(os.path.join(root, issueFile)))
+    for issueFile in os.listdir(os.path.join(root, "issues")):
+        issue = json.load(open(os.path.join(root, "issues", issueFile)))
         closed = issue["closed_at"] if issue["closed_at"] else "2222-01-01T00:00:00Z"
         for comment in issue["comment_data"]:
             if re.search('changed status from .* to "closed"', comment["body"]):
                 closed = comment["created_at"]
         for label in issue.get("labels") or [{}]:
             for assignee in issue.get("assignees") or [{}]:
-                writer.writerow((issue["number"], issue["created_at"], closed, issue["user"]["login"], label.get("name"), label.get("color"), assignee.get("login")))
+                writer.writerow((issue["number"], issue["created_at"], closed, issue["user"]["login"], label.get("name"), assignee.get("login")))
+with open('labels.csv', 'w') as out:
+    print('label,color', file=out)
+    writer = csv.writer(out)
+    for label in json.load(open(os.path.join(root, "labels", "labels.json"))):
+        writer.writerow((label["name"], '#' + label["color"]))
+with open('milestones.csv', 'w') as out:
+    print('milestone,due', file=out)
+    writer = csv.writer(out)
+    for milestoneFile in os.listdir(os.path.join(root, "milestones")):
+        milestone = json.load(open(os.path.join(root, "milestones", milestoneFile)))
+        due = milestone["closed_at"] if milestone["closed_at"] else milestone["due_on"]
+        writer.writerow((milestone["title"], due))
--- dashgraph.py
+++ dashgraph.py
@@ -5,10 +5,18 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
 import pandas as pd
 
-df = pd.read_csv('out.csv', parse_dates=['time', 'closed'])
+issues = pd.read_csv('issues.csv', parse_dates=['time', 'closed'])
+label_colors = pd.read_csv('labels.csv').set_index('label').to_dict()['color']
+milestones = pd.read_csv('milestones.csv', parse_dates=['due'])
+milestone_lines = []
+for milestone in milestones.values:
+    if not pd.isnull(milestone[1]):
+        milestone_lines.append(dict(
+            type='line', yref='paper', y0=0, y1=1,
+            xref='x', x0=milestone[1], x1=milestone[1]
+        ))
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -34,17 +42,27 @@ app.layout = html.Div([
     [Input('label-dropdown', 'value')])
 def update_figure(labels):
     if len(labels) == 0:
-        labels = ["bug"]
-    data = df[df['label']==labels[0]][['number', 'time', 'closed', 'label_color']].drop_duplicates()
+        filtered = issues
+    else:
+        filtered = issues[issues['label']==labels[0]]
+    data = filtered[['issue', 'time', 'closed']].drop_duplicates()
     data['inc'] = 1
     data['dec'] = -1
-    closed = data[['closed', 'dec', 'label_color']][data['closed'] < pd.Timestamp.now()].rename(columns={'closed':'time','dec':'inc'})
-    d = pd.concat([data[['time', 'inc', 'label_color']], closed]).sort_values('time')
-    d['sum'] = d['inc'].cumsum()
-#    print(d)
+    closed = data[['closed', 'dec']][data['closed'] < pd.Timestamp.now()].rename(columns={'closed':'time', 'dec':'inc'})
+    d = pd.concat([data[['time', 'inc']], closed]).sort_values('time')
+    d['count'] = d['inc'].cumsum()
 
-    fig = px.area(d, x="time", y="sum", color="label_color")
-    fig.update_layout(transition_duration=500)
+    if len(labels) == 0:
+        filtered = issues
+        fig = px.area(d, x="time", y="count")
+    else:
+        d['label'] = labels[0]
+#        print(d)
+        fig = px.area(d, x="time", y="count", color='label', color_discrete_map=label_colors)
+    for milestone in milestones.values:
+        if not pd.isnull(milestone[1]):
+            fig.add_annotation(x=milestone[1], y=0, text=milestone[0])
+    fig.update_layout(transition_duration=500, shapes=milestone_lines)
     return fig
 
 
